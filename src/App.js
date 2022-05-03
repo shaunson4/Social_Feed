cc2223661ed5491656e138feb6aeef9459a32c1d
shaunson04 <sgray4106@gmail.com>
2022-05-02 22:08:39 -0500

    Revised CreatePost, DisplayPosts, and App files to include functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { useState } from 'react';
+import DisplayPosts from './Components/DisplayPosts'
+import CreatePosts from './Components/CreatePosts';
 
 function App() {
 
+  const [post, setPosts] = useState([{Name:'name'}, {Post: 'postMessage' }])
 
+  function addNewPost( post)
+    let tempPosts = [ post, ...posts];
+    setPosts(tempPosts);
 
   return (
     <div>
+      <DisplayPosts parentPosts={posts} />
+      <CreatePosts addNewPostProperty = {addNewPost} />
      <h3>Hello World! </h3> 
     </div>
   );
--- src/Components/CreatePosts.jsx
+++ src/Components/CreatePosts.jsx
@@ -1,10 +1,27 @@
+import React, { useState } from 'react';
+
+
 const CreatePosts = (props) => {
+    const {Name, setName} = useState('');
+    const {Post, setPost} = useState('');
+
+    function handleSubmit (event) {
+        event.preventDefault();
+        let newPost ={
+            Name: name,
+            Post: postMessage
+        };
+        console.log(newPost);
+        props.addNewPostProperty(newPost)
+    }
+
     return (  
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>Name</label>
-            <input type = 'string'/>
+            <input type = 'string' value={Name} onChange={(event) => setName (event.target.value)} />
             <label> Post</label>
-            <input type = 'string'/>
+            <input type = 'string' value={Post} onChange={(event) => setPost (event.target.value)} />
+            <button type = 'submit'> Create </button>
         </form>
 
 
